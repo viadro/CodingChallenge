184ec7f36591d03d540e588d6cce175821f4ffe7
pseweryn <piotr.seweryn@coi.gov.pl>
2024-02-29 22:45:12 +0100

    initial commit

---
+++ app/src/main/java/com/seweryn/piotr/codingchallenge/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.seweryn.piotr.codingchallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.seweryn.piotr.codingchallenge.ui.theme.CodingChallengeTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      CodingChallengeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+          Greeting("Android")
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  CodingChallengeTheme {
+    Greeting("Android")
+  }
+}
